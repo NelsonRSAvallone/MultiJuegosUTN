a04687560fabcee6fb9a0ce3973d67db875a3124
NelsonSosa <nelsonrsavallone@gmail.com>
2019-05-11 17:53:15 -0700

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/Pong/VentanaPong.java
+++ src/Pong/VentanaPong.java
@@ -22,6 +22,8 @@ public class VentanaPong extends JFrame {
         setResizable(false);//impide que el usuario modifique el tamaño de la ventana
         lamina = new PanelPong();
         add(lamina);
+        setVisible(true);//Para que la ventana se vea
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Para que se cierre la aplicacion cuando cierro la ventana
        // hilo=new Hilo(lamina);
         //hilo.start();
         lamina.iterarJuego();//Nuestra lamina se va a estar repintando cada 6 milisegundos
